77855bb400c770d2ab0b3516e086d88fc2a64887
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2023-01-04 22:37:12 +0100

    add fonts and app colors

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const primaryBlue = Color(0xFF0D6EFD);
+  static const gray = Color(0xFF6A6A6A);
+  static const primaryLightBlue = Color(0xFF1483C2);
+  static const black = Color(0xFF2B2B2B);
+  static const lightGray = Color(0xFF707B81);
+  static const orange = Color(0xFFFF4C24);
+}
